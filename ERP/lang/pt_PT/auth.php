9b528a6b6c237fa863f43b1e3ab4f39fcece3120
Celso Batista <celsobatista05@gmail.com>
2026-02-14 23:29:12 +0000

    Adicionado pack PT ao laravel

---
+++ ERP/lang/pt_PT/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'As credenciais introduzidas não coincidem com os nossos registos.',
+    'password' => 'A palavra-passe introduzida está incorreta.',
+    'throttle' => 'Demasiadas tentativas de entrada. Por favor, tente novamente em :seconds segundos.',
+];
---
+++ ERP/lang/pt_PT/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'reset'     => 'A sua palavra-passe foi reposta.',
+    'sent'      => 'Enviámos o link de reposição da palavra-passe para o seu email.',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token'     => 'Este código de reposição da palavra-passe é inválido.',
+    'user'      => 'Não conseguimos encontrar um utilizador com esse endereço de email.',
+];
